150a11eccc118c41e97447a6f0525edbef1760bb
Florian Heigl <f.heigl@wartungsfenster.de>
2017-02-13 02:50:53 +0100

    return codes & english xlt
    - adjust to match the wal-e-wrapper in ansible-pgpool (this just sources all the variables for S3 etc)
    - translate to english
    - make sure the return codes are right for nagios
    
    (I think this was invalid as a Check_MK local check and invalid as a Nagios check)

--- check_wale.py
+++ check_wale.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# orig: https://github.com/APSL/postgresql-wal-e-nagios/blob/master/check_wale.py
 ### Check if the backup wal-e postgres is executed in the last day
 
 import subprocess
@@ -6,12 +7,14 @@ import sys
 from datetime import datetime, timedelta
 import time
 
-command = "/usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-list | tail -1"
+command = "/usr/local/bin/wal-e-wrapper backup-list 2>/dev/null| tail -1"
 
 p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 output, err = p.communicate()
 last =  output
 
+# add regex check here matching if the output is really a wal-e line with a date.
+# if not, exit 3
 last_string = last.split()[1][:-5]
 last_datetime = datetime.strptime(last_string, "%Y-%m-%dT%H:%M:%S")
 #print last_datetime
@@ -20,8 +23,8 @@ last_expected = datetime.now() - timedelta(hours=28)
 #print last_expected
 
 if last_datetime < last_expected:
-    print "CRITICAL: Ultimo backup en  wal-e es de mas de un dia, el ultimo es de: %s !" % last_datetime
+    print "CRITICAL - Last backup via wal-e more than a day ago: %s !" % last_datetime
+    sys.exit(2)
 else:
-    print "OK. Backup correcto. Ultimo backup es de: %s" % last_datetime
- 
-
+    print "OK - Backup correct. Last backup is from: %s" % last_datetime
+    sys.exit(0)
